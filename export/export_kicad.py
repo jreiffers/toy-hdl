cd4c36a480f0c80ced303af61b2474c3546386da
Johannes Reifferscheid <j.reifferscheid@gmail.com>
2026-04-11 12:02:09 +0200

    KICAD netlist exporter.

--- export/export_kicad.py
+++ export/export_kicad.py
@@ -1,3 +1,8 @@
+import os
+
+if 'KICAD9_SYMBOL_DIR' not in os.environ:
+    os.environ['KICAD9_SYMBOL_DIR'] = '/usr/share/kicad/symbols'
+
 from absl import app
 from absl import flags
 import cpu.format_pb2 as s
@@ -6,12 +11,129 @@ from skidl import *
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('input', '', 'Serialized netlist file.')
+flags.DEFINE_string('output', '', 'KICAD schematic output file.')
+flags.DEFINE_list('jumpers', '', 'Inputs that are set using jumpers.')
+
+
+def generate_jumper(nets, input):
+    vss_conn, out, vdd_conn = Part(
+        'Connector_Generic',
+        f'Conn_01x{input.bitwidth:02}',
+        dest=TEMPLATE,
+        footprint=
+        f'Connector_PinHeader_2.54mm:PinHeader_1x{input.bitwidth:02}_P2.54mm_Vertical'
+    )(3)
+
+    assert out[1] is not None, out
+    for i in range(input.bitwidth):
+        vss_conn[i + 1] & nets['vss']
+        vdd_conn[i + 1] & nets['vdd']
+        out[i + 1] & nets[f'{input.name}.{i}']
+
+
+def generate_header(nets, header_nets):
+    conn = Part(
+        'Connector_Generic',
+        f'Conn_01x{len(header_nets):02}',
+        dest=TEMPLATE,
+        footprint=
+        f'Connector_PinHeader_2.54mm:PinHeader_1x{len(header_nets):02}_P2.54mm_Horizontal'
+    )()
+    for i, net in enumerate(header_nets):
+        nets[net] & conn[i + 1]
+
+
+def init_skidl_nets(net):
+    nets = {}
+    nets['vdd'] = Net("VDD")
+    nets['vss'] = Net("VSS")
+
+    header_nets = ['vdd', 'vss']
+
+    for input in net.inputs:
+        nets[input.name] = Bus(input.name, input.bitwidth)
+        for i in range(input.bitwidth):
+            nets[f'{input.name}.{i}'] = nets[input.name][i]
+        if input.bitwidth == 1:
+            nets[input.name] = nets[input.name][0]
+        if input.name in FLAGS.jumpers:
+            generate_jumper(nets, input)
+        else:
+            for i in range(input.bitwidth):
+                header_nets.append(f'{input.name}.{i}')
+
+    for output in net.outputs:
+        if len(output.terminals) == 1:
+            nets[output.name] = Net(output.name)
+            header_nets.append(output.name)
+        else:
+            bus = Bus(output.name, len(output.terminals))
+            nets[output.name] = bus
+            for i in range(input.bitwidth):
+                nets[f'{output.name}.{i}'] = bus[i]
+                header_nets.append(f'{output.name}.{i}')
+
+    return nets, header_nets
+
+
+def generate(net):
+    n = Part("Transistor_FET",
+             "2N7002",
+             dest=TEMPLATE,
+             footprint='Package_TO_SOT_SMD:SOT-23')
+    p = Part("Transistor_FET", "Si2319CDS", dest=TEMPLATE)
+
+    nets, header_nets = init_skidl_nets(net)
+
+    transistors = []
+    for id, t in enumerate(net.transistors):
+        # TODO: Chip selection - if src is not vss/vdd, a CD4007 is probably needed.
+        if t.kind == s.Transistor.Kind.kNChannel:
+            transistor = n()
+        else:
+            transistor = p()
+
+        assert transistor["G"] is not None, transistor
+        assert transistor["S"] is not None, transistor
+        assert transistor["D"] is not None, transistor
+
+        transistor.tag = f't{id}'
+        transistors.append(transistor)
+        nets[f"{id}.g"] = transistor["G"]
+        nets[f"{id}.s"] = transistor["S"]
+        nets[f"{id}.d"] = transistor["D"]
+
+    vddt = Part("power", "VDD")
+    vsst = Part("power", "VSS")
+
+    vddt & nets['vdd']
+    vsst & nets['vss']
+
+    def get_net(name):
+        assert name in nets, f'net {name} not found'
+        net = nets[name]
+        assert net is not None, f'net {name} is None'
+        return net
+
+    for c in net.connections:
+        a = c.node_a
+        b = c.node_b
+
+        get_net(a) & get_net(b)
+
+    generate_header(nets, header_nets)
+    generate_netlist(tool=KICAD9, file=FLAGS.output)
+
 
 def main(argv):
+    assert len(FLAGS.input) > 0
+    assert len(FLAGS.output) > 0
+
     with open(FLAGS.input, "rb") as f:
-         net = s.Network()
-         net.ParseFromString(f.read())
-         print(net)
+        net = s.Network()
+        net.ParseFromString(f.read())
+        generate(net)
+
 
 if __name__ == '__main__':
     app.run(main)
